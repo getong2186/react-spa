40e74c3c0137c68dae6ff7bbf4281ad59bae8768
getong <getong2186@gmail.com>
2018-12-10 16:25:36 +0800

    fix：react-route切换渐变效果

--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,7 +1,32 @@
 import React,{ Component } from 'react';
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
+import style from "./Container.scss";
 export default class App extends Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+ 
+    componentWillMount() {
+        document.body.style.margin = "0px";
+        // 这是防止页面被拖拽
+        document.body.addEventListener('touchmove', (ev) => {
+            ev.preventDefault();
+        });
+    }
   render() {
-    const { children } =this.props;
-    return children
+    // const { children } =this.props;
+    return  <ReactCSSTransitionGroup
+                transitionName="transitionWrapper"
+                component="div"
+                className={style.transitionWrapper}
+                transitionEnterTimeout={300}
+                transitionLeaveTimeout={300}>
+                <div key={this.props.location.pathname}
+                     style={{position:"absolute", width: "100%"}}>
+                    {
+                        this.props.children
+                    }
+                </div>
+            </ReactCSSTransitionGroup>
   }
 }
