7b8a411433b94f0f04ecea8d182f04bcb86b092c
getong <getong2186@gmail.com>
2018-12-17 16:03:40 +0800

    fix：react定版

--- src/js/containers/404.js
+++ src/js/containers/404.js
@@ -6,7 +6,7 @@ export default class Swa extends Component {
     return (
       <div className="center-center-column" style={{height: '100vh'}}>
         <span className="font-26">404!页面没有找到，请</span>
-        <Link to="/" className="font-36 main-color">返回首页</Link>
+        <Link to="/" className="font-36 main-color">返回</Link>
       </div>
     )
   }
--- src/js/containers/App.js
+++ src/js/containers/App.js
@@ -1,11 +1,10 @@
 import React,{ Component } from 'react';
 import ReactCSSTransitionGroup from "react-addons-css-transition-group";
-import style from "./Container.scss";
+import style from "./app.scss";
 export default class App extends Component {
     constructor(props, context) {
         super(props, context);
     }
- 
     componentWillMount() {
         document.body.style.margin = "0px";
         // 这是防止页面被拖拽
